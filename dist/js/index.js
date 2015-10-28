456119ed0919ae1828aa416c41d0ab6456e91711
Ian Ross <ian.ackerman.ross@gmail.com>
2015-10-28 13:13:50 -0500

    Add nlp_352 metrics, a few colors

--- dist/js/index.js
+++ dist/js/index.js
@@ -30,6 +30,7 @@
       ocr: journals.map(function(d) { return d.ocr }).reduce(function(a, b) { return a + b}, 0),
       cuneiform: journals.map(function(d) { return d.cuneiform }).reduce(function(a, b) { return a + b}, 0),
       nlp: journals.map(function(d) { return d.nlp }).reduce(function(a, b) { return a + b}, 0),
+      nlp352: journals.map(function(d) { return d.nlp352 }).reduce(function(a, b) { return a + b}, 0),
       fonttype: journals.map(function(d) { return d.fonttype }).reduce(function(a, b) { return a + b}, 0)
     }
   }
@@ -44,7 +45,7 @@
   var template = $('#template').html();
   Mustache.parse(template);
 
-  var colors = ['#2D95BF', '#955BA5', '#112F41', '#068587', '#4FB99F', '#F2B134', '#ED553B', '#DBC73D', '#C44E18', '#61B594', '#C40076', '#16005A', '#C3E92E', '#363535', '#DB6E73', '#FFBB6A', '#46906E', '#175882', '#A1B4BA', '#3392B8', '#4C4C4C', '#62B072', '#2B1D2E', '#323657', '#076473', '#54B087', '#D6F567', '#6D8C75', '#D9AD77', '#BF7B75', '#74DDB8', '#F2DFA7', '#D99F59', '#D9782B', '#5C474C', '#7D736A', '#54656B', '#B04C2E'];
+  var colors = ['#2D95BF', '#955BA5', '#112F41', '#068587', '#4FB99F', '#F2B134', '#ED553B', '#DBC73D', '#C44E18', '#61B594', '#C40076', '#16005A', '#C3E92E', '#363535', '#DB6E73', '#FFBB6A', '#46906E', '#175882', '#A1B4BA', '#3392B8', '#4C4C4C', '#62B072', '#2B1D2E', '#323657', '#076473', '#54B087', '#D6F567', '#6D8C75', '#D9AD77', '#BF7B75', '#74DDB8', '#F2DFA7', '#D99F59', '#D9782B', '#5C474C', '#7D736A', '#54656B', '#B04C2E', '#35181E', '#1D3826', '#AB664F'];
 
   $.getJSON('https://geodeepdive.org/api/v1/metrics?all', function(data) {
     var results = data.success.data.metrics;
