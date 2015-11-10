53df58b9e2d39ef623babb02665d5e3f4127d2b5
John J Czaplewski <jczaplew@gmail.com>
2015-11-10 10:34:15 -0600

    Fixes and moar colors

--- dist/js/index.js
+++ dist/js/index.js
@@ -8,7 +8,11 @@
     }
 
     if (isJournal) {
-      data["journal"] = "journal"
+      data["journal"] = "journal";
+    }
+
+    if (name === 'Elsevier' || name === 'USGS') {
+      data["source"] = "Publisher";
     }
 
     Object.keys(data).forEach(function(d) {
@@ -41,10 +45,14 @@
     return parts.join(".");
   }
 
+  function randomColor() {
+    return colors[Math.floor(Math.random() * (colors.length - 1))];
+  }
+
   var template = $('#template').html();
   Mustache.parse(template);
 
-  var colors = ['#2D95BF', '#955BA5', '#112F41', '#068587', '#4FB99F', '#F2B134', '#ED553B', '#DBC73D', '#C44E18', '#61B594', '#C40076', '#16005A', '#C3E92E', '#363535', '#DB6E73', '#FFBB6A', '#46906E', '#175882', '#A1B4BA', '#3392B8', '#4C4C4C', '#62B072', '#2B1D2E', '#323657', '#076473', '#54B087', '#D6F567', '#6D8C75', '#D9AD77', '#BF7B75', '#74DDB8', '#F2DFA7', '#D99F59', '#D9782B', '#D9782B', '#5C474C', '#7D736A', '#54656B', '#B04C2E'];
+  var colors = _.shuffle(['#2D95BF', '#357BCB', '#955BA5', '#112F41', '#36424A', '#068587', '#4FB99F', '#F2B134', '#ED553B', '#DBC73D', '#C44E18', '#61B594', '#C40076', '#591C00', '#455C7B', '#16005A', '#57385C', '#363535', '#DB6E73', '#005D5C', '#FFBB6A', '#46906E', '#175882', '#A1B4BA', '#3392B8', '#4C4C4C', '#053462', '#62B072', '#2B1D2E', '#323657', '#076473', '#54B087', '#6D8C75', '#D9AD77', '#3F3E05', '#BF7B75', '#74DDB8', '#D99F59', '#D9782B', '#D9782B', '#5C474C', '#7D736A', '#54656B', '#742C4E', '#B04C2E']);
 
   $.getJSON('https://geodeepdive.org/api/v1/metrics?all', function(data) {
     var results = data.success.data.metrics;
@@ -60,16 +68,17 @@
 
     // Report the top journals
     delete results.total;
-    var top10 = _.sortBy(
+
+    _.sortBy(
       Object.keys(results)
         .map(function(d) {
           return results[d]
         })
       , 'fetched')
       .reverse()
-      .slice(0, colors.length - 1)
+      .slice(0, colors.length * 2)
       .forEach(function(d, i) {
-        $('.wrapper').append(parseContent(d, d.nospace.replace(/_/g, ' '), 't' + (i + 1), colors[i], true))
+        $('.wrapper').append(parseContent(d, d.nospace.replace(/_/g, ' '), 't' + (i + 1), randomColor(), true))
       });
 
   });
